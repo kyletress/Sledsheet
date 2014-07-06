f20eb7c55ac2a8d65b3a06a4370e9b5f523d5c47
Kyle Tress <kyletress@gmail.com>
2014-07-06 15:26:54 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sledsheet::Application.config.secret_key_base = '67b7207463612c58d87ba5b9fec3fe7038fd6fe7663a1ac070d50a26cc6adb0d27f2cb9cbb9cb4fcbcc7f0f6d44138396923a7b2a2c1f86c1eb1e276095f4542'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sledsheet::Application.config.session_store :cookie_store, key: '_sledsheet_session'
