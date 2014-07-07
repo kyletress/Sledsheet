457235616563df14f1e56d5ba7c0eadd9a14413f
Kyle Tress <kyletress@gmail.com>
2014-07-07 16:26:00 -0400

    Update to production.rb, another attempt at Heroku solution

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,14 @@ Sledsheet::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = true # Heroku?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true # Heroku?
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -38,7 +38,7 @@ Sledsheet::Application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-  config.action_dispatch.x_sendfile_header = nil # for heroku?
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for heroku?
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
