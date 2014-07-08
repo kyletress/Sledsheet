f3f3fb5ba9d3f3d7a536f9f5bd371b82f346ae27
Kyle Tress <kyletress@gmail.com>
2014-07-07 22:18:10 -0400

    Finish sign in

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Sledsheet::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true # Heroku?
+  config.assets.compile = false # Heroku?
 
   # Generate digests for assets URLs.
   config.assets.digest = true
