8c41018e489a34c294161df9889412fa122bd325
Kyle Tress <kyletress@gmail.com>
2015-03-13 23:01:17 +0000

    Add display_time to application helper and track show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,10 @@ module ApplicationHelper
   def nice_date(date)
     # format a date, nicely
   end
+  
+  def display_time(i)
+    unless i.nil?
+      sprintf("%.2f", i / 100.00)
+    end
+  end
 end
