ece7c06f15694ab2b65052a03cb1ecb64760ea67
Kyle Tress <kyle@kyletress.com>
2015-06-13 19:16:00 -0400

    Run tests and move admin menu

--- test/controllers/athletes_controller_test.rb
+++ test/controllers/athletes_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class AthletesControllerTest < ActionController::TestCase
-  
+
   setup do
     @admin = users(:kyle)
     @user = users(:matt)
     @athlete = athletes(:kyle)
   end
-  
+
   test "should get index" do
     get :index
     assert_response :success
@@ -19,49 +19,48 @@ class AthletesControllerTest < ActionController::TestCase
       assert_select 'a[href=?]', athlete_path(athlete), text: athlete.name
     end
   end
-  
+
   test "should show athlete" do
     get :show, id: @athlete
     assert_response :success
     assert_template :show
     assert_select "title", "Sledsheet | #{@athlete.first_name} #{@athlete.last_name}"
   end
-  
+
   test "admin should get new" do
     log_in_as(@admin)
     get :new
     assert_response :success
     assert_select "title", "Sledsheet | New Athlete"
   end
-  
+
   test "user should not get new" do
     log_in_as @user
     get :new
     assert_redirected_to root_path
   end
-  
+
   test "admin should get edit" do
     log_in_as @admin
     get :edit, id: @athlete
     assert_response :success
     assert_select "title", "Sledsheet | Edit #{@athlete.name}"
   end
-  
+
   test "user should not get edit" do
     log_in_as @user
     get :edit, id: @athlete
     assert_redirected_to root_url
   end
-  
+
   test "should create athlete" do
     log_in_as @admin
     assert_difference('Athlete.count') do
       post :create, athlete: { first_name: 'Morgan', last_name: 'Tracey', country_code: 'US' }
     end
-
     assert_redirected_to athlete_path(assigns(:athlete))
   end
-  
+
   test "should update athlete" do
     log_in_as @admin
     patch :update, id: @athlete, athlete: { first_name: 'John', last_name: 'Tress', country_code: 'US' }
--- test/controllers/runs_controller_test.rb
+++ test/controllers/runs_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class RunsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @admin = users(:kyle)
+    @user = users(:matt)
+    @run = runs(:kyle1)
+  end
+
+  test "admin should get edit" do
+    log_in_as @admin
+    get :edit, id: @run
+    assert_response :success
+    assert_template 'runs/edit'
+    assert_not_nil assigns(:run)
+    assert_select "title", "Sledsheet | Edit Run"
+  end
+
+  test "user should not get edit" do
+    log_in_as @user
+    get :edit, id: @run
+    assert_redirected_to root_path
+  end
+
 end
