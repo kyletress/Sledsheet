0df0fa6d08c6edfbc4533c0e9c3f7066fd0f4eb8
Kyle Tress <kyle@kyletress.com>
2015-07-23 22:26:55 -0400

    Update mailer language

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
   def invitation(invitation)
     @invitation = invitation
-    mail(to: @invitation.recipient_email, subject: "You've been invited to Sledsheet")
+    mail(to: @invitation.recipient_email, subject: "You're invited to Sledsheet")
     invitation.update_attributes(sent_at: Time.now)
   end
 
