7276aa906783e0c759ca6e62b788205722fce9cd
Kyle Tress <kyle@kyletress.com>
2015-11-26 12:59:18 -0500

    Update Gibbon to work with Mailchimp API 3.0

--- app/jobs/subscribe_user_to_mailing_list_job.rb
+++ app/jobs/subscribe_user_to_mailing_list_job.rb
@@ -2,7 +2,8 @@ class SubscribeUserToMailingListJob < ActiveJob::Base
   queue_as :default
 
   def perform(user)
-    gb = Gibbon::API.new
+    gb = Gibbon::Request.new
     gb.lists.subscribe({id: ENV['MAILCHIMP_LIST_ID'], email: {email: user.email}, double_optin: false})
+    gb.lists(ENV['MAILCHIMP_LIST_ID']).members.create(body: {email_address: user.email, status: "subscribed"}, double_optin: false)
   end
 end
