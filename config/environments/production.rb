8745365b080c3babc91c16fe0a7906d9018e7569
Kyle Tress <kyle@kyletress.com>
2016-03-07 15:38:34 -0500

    Make cloudfront work with heroku review apps

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,9 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV["ASSET_HOST"]
+  unless ENV["REVIEW_ENVIRONMENT"] == "true"
+    config.action_controller.asset_host = ENV["ASSET_HOST"]
+  end
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
