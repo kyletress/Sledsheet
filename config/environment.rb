daf4c1018f5098b7a398ff0b7ea21ebdd07b9c8f
Kyle Tress <kyle@kyletress.com>
2016-09-04 23:10:18 -0700

    bring my custom date format back

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Time::DATE_FORMATS[:starts_at] = lambda do |date|
+  date.strftime "%B #{ActiveSupport::Inflector.ordinalize(date.day)}, %-l:%M%P"
+end
