001aee90cb14f26b6bc9f19cc6d2dcee35c79e2f
Kyle Tress <kyle@kyletress.com>
2017-02-17 23:07:07 -0700

    clean up article import, and limit articles from the same sources

--- app/jobs/scrape_athlete_articles_from_google_job.rb
+++ app/jobs/scrape_athlete_articles_from_google_job.rb
@@ -3,7 +3,7 @@ class ScrapeAthleteArticlesFromGoogleJob < ApplicationJob
 
   def perform(athlete)
     return unless athlete.rss_alert_url.present?
-    doc = Nokogiri::XML(open(athlete.rss_alert_url))
+    doc = Nokogiri::XML.parse(open(athlete.rss_alert_url))
     doc.css('entry').each do |entry|
       article = Article.from_google_alerts(entry, athlete)
       article.save
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,13 @@
 class Article < ApplicationRecord
   belongs_to :athlete
 
+  validates :title, uniqueness: { scope: :athlete_id }
+
   def self.from_google_alerts(entry, athlete)
     article = Article.new(
-      title: entry.css('title').inner_html,
-      link: entry.css('link').inner_html,
-      description: entry.css('content').inner_html,
+      title: entry.css('title').text,
+      link: entry.css('link').attribute('href').value,
+      description: entry.css('content').text,
       athlete: athlete
     )
     article
